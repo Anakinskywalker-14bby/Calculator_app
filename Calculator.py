875a411443c3e73ce1f29b8c461974bb4132be46
Khaleed <z5600243@ad.unsw.edu.au>
2025-07-05 17:22:24 +1000

    Initialzed the user display

--- Calculator.py
+++ Calculator.py
@@ -6,5 +6,11 @@ root = tk.TK()
 root.title("Calculator")
 root.geometry("400x600")
 
+#User display
+entry_variable = tk.StringVar()
+entry = tk.Entry(root, textvariable=entry_variable, font="Arial 18",
+                 justify = "right", bd = 10, relief = tk.SUNKEN)
+entry.pack(fill = tk.BOTH, ipadx = 8, ipady = 8, padx = 10, pady = 10)
+
 # start the Tkinter event loop
 root.mainloop()
