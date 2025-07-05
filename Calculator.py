1d7841ad911b1ef6e22e7b36e747573e40a6ed85
Khaleed <z5600243@ad.unsw.edu.au>
2025-07-05 17:11:55 +1000

    Initialzed the root window

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# initialze the root window
+root = tk.TK()
+root.title("Calculator")
+root.geometry("400x600")
+
+# start the Tkinter event loop
+root.mainloop()
